b1c1cfe17c13f92822a9b7115af92b271e53f825
dustin <dustinusey@me.com>
2024-06-03 11:31:52 -0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="grid place-items-center h-screen bg-zinc-300">
+      <div className="bg-white rounded-lg px-5 py-10 w-full max-w-[500px] text-zinc-500 text-xl text-center">
+        <h1 className="text-3xl font-bold">Hello, World!</h1>
+      </div>
+    </div>
+  );
+};
+export default App;
